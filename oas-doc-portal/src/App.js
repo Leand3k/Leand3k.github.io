cef1716d0ed0daca8b01085c4f3cd803c45c8c3d
Leand3k <theleand3000@gmail.com>
2022-03-28 15:26:48 -0400

    Dockerfile done.

--- oas-doc-portal/src/App.js
+++ oas-doc-portal/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SwaggerUI from "swagger-ui";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			definitionLink:
+				"https://api-ecf-services-o3dqoakkaa-ue.a.run.app/openapi.json",
+		};
+	}
+
+	componentDidMount() {
+		SwaggerUI({
+			domNode: document.getElementById("api-data"),
+			url: this.state.definitionLink,
+		
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div id="api-data" />
+			</div>
+		);
+	}
 }
 
 export default App;
