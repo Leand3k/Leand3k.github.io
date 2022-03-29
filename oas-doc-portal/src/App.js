aed04a544ad17cf17f9522058f7bb9d2042d5db0
Leand3k <theleand3000@gmail.com>
2022-03-29 13:40:23 -0400

    Links working.

--- oas-doc-portal/src/App.js
+++ oas-doc-portal/src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SwaggerUI from "swagger-ui";
-import React, { Component } from "react";
+import React, {
+	Component
+} from "react";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			definitionLink:
-				"https://api-ecf-services-o3dqoakkaa-ue.a.run.app/openapi.json",
+			definitionLink: "http://localhost:8083/openapi.json",
 		};
 	}
 
@@ -16,15 +17,38 @@ class App extends Component {
 		SwaggerUI({
 			domNode: document.getElementById("api-data"),
 			url: this.state.definitionLink,
-		
 		});
 	}
 
+	swaggerhub(inputMethod, inputResource, inputParams) {
+		let url = ""
+		if (inputParams) {
+			url = "https://api.swaggerhub.com/apis/" + inputResource + "?" + inputParams
+		} else {
+			url = "https://api.swaggerhub.com/apis/" + inputResource
+		}
+
+		return fetch(url, {
+			method: inputMethod
+		}).then(response => {
+			if (response.ok) {
+				return response.json()
+			}
+			throw new Error('There was an issue requesting the API')
+		}).then(json => {
+			return json
+		})
+	}
+
+
+
 	render() {
-		return (
-			<div className="App">
-				<div id="api-data" />
-			</div>
+		return ( <
+			div className = "App" >
+			<
+			div id = "api-data" / >
+			<
+			/div>
 		);
 	}
 }
