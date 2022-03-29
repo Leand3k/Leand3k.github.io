f2c4e29d7101ba949d15500838a539081927fcb5
Leand3k <theleand3000@gmail.com>
2022-03-29 15:58:53 -0400

    Implementing links.

--- oas-doc-portal/src/App.js
+++ oas-doc-portal/src/App.js
@@ -1,54 +1,91 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SwaggerUI from "swagger-ui";
-import React, {
-	Component
-} from "react";
+import React, { Component } from "react";
+import Config from "./organization_config.json";
+import Sidebar from "./Sidebar.js";
+import "../node_modules/swagger-ui/dist/swagger-ui.css";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			definitionLink: "http://localhost:8083/openapi.json",
-		};
+			organizationConfig: null,
+			definitionList: null,
+			definitionLink: "https://petstore.swagger.io/v2/swagger.json"
+		}
+		this.swaggerhub = this.swaggerhub.bind(this)
+		this.getOrganizationData = this.getOrganizationData.bind(this)
+		this.updateDefinitionLink = this.updateDefinitionLink.bind(this)
 	}
 
+	// Mounting each api
 	componentDidMount() {
 		SwaggerUI({
 			domNode: document.getElementById("api-data"),
 			url: this.state.definitionLink,
-		});
+		});	
 	}
 
 	swaggerhub(inputMethod, inputResource, inputParams) {
-		let url = ""
+		let url = "";
 		if (inputParams) {
-			url = "https://api.swaggerhub.com/apis/" + inputResource + "?" + inputParams
+			url =
+				"https://api.swaggerhub.com/apis/" + inputResource + "?" + inputParams;
 		} else {
-			url = "https://api.swaggerhub.com/apis/" + inputResource
+			url = "https://api.swaggerhub.com/apis/" + inputResource;
 		}
 
 		return fetch(url, {
-			method: inputMethod
-		}).then(response => {
-			if (response.ok) {
-				return response.json()
-			}
-			throw new Error('There was an issue requesting the API')
-		}).then(json => {
-			return json
+			method: inputMethod,
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error("There was an issue requesting the API");
+			})
+			.then((json) => {
+				return json;
+			});
+	}
+
+	// Get organizations per input page.
+	getOrganizationData(organization) {
+		let inputParams = "page=0&limit=20&sort=NAME&order=ASC";
+		let inputResource = organization;
+
+		this.swaggerhub("GET", inputResource, inputParams).then((response) => {
+			this.setState({
+				definitionList: response.apis,
+			});
+		});
+	}
+
+	// handle mapping the data as the page loads
+	componentWillMount() {
+		this.setState({
+			organizationConfig: Config.orgData,
+		});
+	}
+
+	updateDefinitionLink(newLink) {
+		this.setState({
+			definitionLink: newLink
 		})
 	}
 
-
-
 	render() {
-		return ( <
-			div className = "App" >
-			<
-			div id = "api-data" / >
-			<
-			/div>
+		return (
+			<div className="App">
+				<Sidebar
+					organizationConfig={this.state.organizationConfig}
+					definitionList={this.state.definitionList}
+					updateDefinitionLink={this.updateDefinitionLink}
+					getOrganizationData={this.getOrganizationData}
+				/>
+				<div id="api-data" />
+			</div>
 		);
 	}
 }
